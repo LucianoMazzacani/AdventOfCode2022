8ff9f4069bba1da4a58a80076259a2322094e1b9
LPLMazzacani <L.Mazzacani@visio.nl>
2023-10-30 11:54:40 +0100

    UI aangepast in Day25

--- CodeChallenges/Day25/Program.cs
+++ CodeChallenges/Day25/Program.cs
@@ -2,14 +2,29 @@
 using Day25.NumberConversion;
 using TextReaders;
 
-var fuelRequirementsReader = new TextFileReader { Source = @"Data\FuelRequirements.txt" };
-var numberToSnafuConvertor = new NumberToSnafuConvertor();
-var snafuToNumberConvertor = new SnafuToNumberConvertor();
+Console.Write($@"
+Day 25 Bereken in SNAFU hoeveel brandstof eenheden nodig zijn.
 
-var fuelNeeded = fuelRequirementsReader
-    .ReadAllLines()
-    .Select(snafuNumber => snafuToNumberConvertor.Convert(snafuNumber))
-    .Sum();
+");
 
-var amountOfFuelNeededInSnafuNotation = numberToSnafuConvertor.Convert(fuelNeeded);
-Console.WriteLine($"{amountOfFuelNeededInSnafuNotation} brandstofeenheden nodig (in snafu-units)");
+
+try
+{
+    var fuelRequirementsReader = new TextFileReader { Source = @"Data\FuelRequirements.txt" };
+    var numberToSnafuConvertor = new NumberToSnafuConvertor();
+    var snafuToNumberConvertor = new SnafuToNumberConvertor();
+
+    var fuelNeeded = fuelRequirementsReader
+        .ReadAllLines()
+        .Select(snafuNumber => snafuToNumberConvertor.Convert(snafuNumber))
+        .Sum();
+
+    var amountOfFuelNeededInSnafuNotation = numberToSnafuConvertor.Convert(fuelNeeded);
+    Console.WriteLine($"{amountOfFuelNeededInSnafuNotation} brandstofeenheden nodig (in snafu-units)");
+}
+catch (Exception exception)
+{
+    Console.Write("Er is een fout opgetreden", exception);
+}
+Console.WriteLine("\r\nDruk op een toets.");
+Console.ReadKey();
